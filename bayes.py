a0cf43beeca46ca1b31ca6b96cea843ce2e66d3a
Daniela Vargas <danvargas94@hotmail.com>
2017-03-12 16:49:54 -0600

    Bayes network

--- bayes.py
+++ bayes.py
@@ -1,4 +1,5 @@
 import itertools
+import copy
 import fileinput
 
 class Node:
@@ -8,18 +9,9 @@ class Node:
 		self.childs = childs
 		self.table = table
 
-	def getParentNodes(self):
-		return self.parents
-
 	def __repr__(self):
 		return "Node(%s %s %s %s)" % (self.value, self.parents, self.childs, self.table)
 
-def parseNodes(nodes_array):
-	nodes = []
-	for value in nodes_array:
-		for node in value:
-			nodes.append(node)
-	return nodes
 
 def getParents(parents):
 	for parent in parents:
@@ -33,70 +25,67 @@ def getParents(parents):
 								parents.append(parent)
 	return parents
 
-def parseQueries():
-	for query in queries:
-		if query.count('|') > 0: 
-			search = query[:query.find('|')]
-			evidence = query[query.find('|') + 1:]
-		else:
-			matches = filter(lambda obj: obj.value == query.replace('+', '-').replace('-',''), bayesNetwork)
-			if matches and query in matches[0].table:
-				print((matches[0].table)[query])
-			else:
-				given = query
-				parents = getParents(matches[0].parents)
-				combinations = getCombinations(given, parents)
-				suma = 0.0
-				for com in combinations:
-					suma += getChainRule(com)
-				print(suma)
 
 def getCombinations(given, parents):
-	total = []
-	if len(parents) > 1:
-		new = list(itertools.product(*((x, -x) for x in range(1, len(parents) + 1))))
-		for i in new:
-			arr = [given]
-			for j in i:
-				if j > 0:
-					arr.append('+' + parents[j - 1])
-				elif j != 0:
-					arr.append('-' + parents[(-j) - 1])
-			total.append(arr)
+	if type(given) != list:
+		given = [given]
+	if parents:
+		total = []
+		if len(parents) > 1:
+			new = list(itertools.product(*((x, -x) for x in range(1, len(parents) + 1))))
+			for i in new:
+				arr = []
+				for j in i:
+					if j > 0:
+						arr.append('+' + parents[j - 1])
+					elif j != 0:
+						arr.append('-' + parents[(-j) - 1])
+				total.append(arr)
+		else:
+			total.append('+' + parents[0])
+			total.append('-' + parents[0])
+
+		return total
 	else:
-		arr = [given]
-		arr.append('+' + parents[0])
-		total.append(arr)
-		arr = [given]
-		arr.append('-' + parents[0])
-		total.append(arr)
+		return []
 
-	return total
-
-def getChainRule(combination):
+def getChainRule(combination, originalCombinations):
 	total = 1.0
+	#print("Combination", combination)
 	if combination:
-		values = combination[0]
-		for value in combination:
-			valueCopy = value[1:]
-			node = (filter(lambda x: x.value == valueCopy , bayesNetwork))
-			parents = node[0].parents
-			if parents:
-				for index, parent in enumerate(sorted(parents)):
-					if index == 0:
-						values += '|'
-					if '+' + parent in combination:
-						values += '+' + parent
-					else:
-						values += '-' + parent
-					if index != len(parents) - 1:
-						values += ","
-			probabilities = node[0].table
-			if values in probabilities:
-				total = total * probabilities[values] * getChainRule(combination[1:])
+		search = combination[0]
+		node = (filter(lambda x: x.value == search[1:] , bayesNetwork))
+		values = search
+		parents = node[0].parents
+		if parents:
+			for index, parent in enumerate(sorted(parents)):
+				if index == 0:
+					values += '|'
+				if '+' + parent in originalCombinations:
+					values += '+' + parent
+				else:
+					values += '-' + parent
+				if index < len(parents) - 1:
+					values += ","
+		probabilities = node[0].table
+		#print("Values", values)
+		if values in probabilities:
+			#print("Probability", values, probabilities[values])
+			total = total * probabilities[values] * getChainRule(combination[1:], originalCombinations)
 	return total
 
+def parseNodes(nodes_array):
+	nodes = []
+	for value in nodes_array:
+		for node in value:
+			nodes.append(node)
+	return nodes
 
+def orderProbability(query, evidence):
+	query = sorted(query.split(','), key=lambda x: x[1:])
+	evidence = sorted(evidence.split(','), key=lambda x: x[1:])
+	query = ",".join(query) + '|' + ",".join(evidence)
+	return query
 
 def parseProbabilities(nodes, probabilities):
 	for node in nodes:
@@ -111,11 +100,19 @@ def parseProbabilities(nodes, probabilities):
 			for ch in parent:
 				parents.append(((ch[ch.find('|') + 1:ch.find('=')]).replace('+','').replace('-','')).split(','))
 				given = float((ch[ch.find('=') + 1:]))
-				probability[(ch[0:ch.find('=')])] = given
+				givenProb = (ch[0:ch.find('=')])
+				query = (givenProb[0:givenProb.find('|')]) 
+				evidence = (givenProb[givenProb.find('|') + 1: ch.find('=')])
+				probability[orderProbability(query, evidence)] = given
+		
 				if (ch[0:ch.find('|')]).count('+') > 0:
-					probability[(ch[0:ch.find('|')]).replace('+', '-') + '|'  + (ch[ch.find('|') + 1: ch.find('=')])] = round(1.0 - given, 2)
+					query = (ch[0:ch.find('|')]).replace('+', '-') 
+					evidence = (ch[ch.find('|') + 1: ch.find('=')])
+					probability[orderProbability(query, evidence)] = round(1.0 - given, 2)
 				else:
-					probability[(ch[0:ch.find('|')]).replace('-', '+') + '|'  + (ch[ch.find('|') + 1: ch.find('=')])] = round(1.0 - given, 2)
+					query = (ch[0:ch.find('|')]).replace('-', '+') 
+					evidence = (ch[ch.find('|') + 1: ch.find('=')])
+					probability[orderProbability(query, evidence)] = round(1.0 - given, 2)
 		else:
 			probability = {}
 			for ch in parent:
@@ -132,6 +129,72 @@ def parseProbabilities(nodes, probabilities):
 					childrens.append((ch[0:ch.find('|')].replace('+','').replace('-','')).split(','))
 					
 		bayesNetwork.append(Node(node, list(set(list(itertools.chain.from_iterable(parents)))), list(set(list(itertools.chain.from_iterable(childrens)))),probability))
+
+def parseQueries(bayesNetworkCopy):
+	for query in queries:
+		#print("-------------", query, "-------------")
+		if query.count('|') > 0: 
+			numerator = []
+			numeratorParents = []
+			denominator = []
+			denominatorParents = []
+			search = (query[:query.find('|')]).split(',')
+			evidence = (query[query.find('|') + 1:]).split(',')
+			for s in search:
+				numerator.append(s[1:])
+			for e in evidence:
+				search.append(e)
+				numerator.append(e[1:])
+				denominator.append(e[1:])
+			for n in numerator:
+				matches = filter(lambda obj: obj.value == n, bayesNetworkCopy)
+				parents = getParents(matches[0].parents)
+				numeratorParents.append(parents)
+				if n in denominator:
+					denominatorParents.append(parents)
+			numeratorParents = list(set(list(itertools.chain.from_iterable(numeratorParents))))
+			denominatorParents = list(set(list(itertools.chain.from_iterable(denominatorParents))))
+			numeratorParents = list(set(numeratorParents) - set(numerator))
+			denominatorParents = list(set(denominatorParents) - set(denominator))
+			upperCombinations = getCombinations(search, numeratorParents)
+			downCombinations = getCombinations(evidence, denominatorParents)
+			#print("given", search, evidence)
+			upperSum = 0.0
+			downSum = 0.0
+
+			if upperCombinations:
+				#print("len com", len(upperCombinations), upperCombinations)
+				for com in upperCombinations:
+					if type(com) != list:
+						com = [com]
+					probability = ",".join(search) + "," + ",".join(com)
+					upperSum += getChainRule(list(set(probability.split(','))), list(set(probability.split(','))))
+			else:
+				upperSum += getChainRule(list(search), list(search))
+			if downCombinations:
+				#print("len com down", len(downCombinations), downCombinations)
+				for com in downCombinations:
+					if type(com) != list:
+						com = [com]
+					probability = ",".join(evidence) + "," + ",".join(com)
+					downSum += getChainRule(list(set(probability.split(','))), list(set(probability.split(','))))
+			else:
+				downSum += getChainRule(list(evidence), list(evidence))
+			print(('%.7f'%(upperSum/downSum)).rstrip('0'))
+		else:
+			matches = filter(lambda obj: obj.value == query.replace('+', '-').replace('-',''), bayesNetwork)
+			if matches and query in matches[0].table:
+				print((matches[0].table)[query])
+			else:
+				given = query
+				parents = getParents(matches[0].parents)
+				combinations = getCombinations(given, parents)
+				suma = 0.0
+				for com in combinations:
+					if type(com) != list:
+						com = [com]
+					suma += getChainRule([given] + com, [given] + com)
+				print(('%.7f'%suma).rstrip('0'))
 
 
 nodes = []
@@ -162,8 +225,10 @@ for line in input:
 
 final_nodes = parseNodes(nodes)
 parseProbabilities(final_nodes, probabilities)
-for val in bayesNetwork:
-	print(val.table)
-parseQueries()
+bayesNetworkCopy = copy.deepcopy(bayesNetwork)
+parseQueries(bayesNetworkCopy)
+
+
+
 
 
