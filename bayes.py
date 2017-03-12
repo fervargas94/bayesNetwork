0f7a2ab9a8dc979c220766aebcad45e67ab1a7a6
Daniela Vargas <danvargas94@hotmail.com>
2017-03-12 17:55:06 -0600

    Different input

--- bayes.py
+++ bayes.py
@@ -204,7 +204,7 @@ getting = 0
 input = fileinput.input()
 bayesNetwork = []
 
-for line in input:
+'''for line in input:
 	if not line and getting == "q":
 		break
 	if getting == "n":
@@ -221,7 +221,27 @@ for line in input:
 	if line == '[Nodes]\n':
 		getting = "n"
 	if line == '[Queries]\n':
-		getting = "q"
+		getting = "q"'''
+
+while 1:
+    line = raw_input()
+    if not line and getting == "q":
+        break
+    if getting == "n":
+    	if line != "" and line[0] != "[" and line[0] != "#":
+    		nodes.append((line.replace(' ', '')).split(','))
+    if getting == "p":
+    	if line != "" and line[0] != "[" and line[0] != "#":
+    		probabilities.append((line.rstrip('\n')).replace(' ', ''))
+    if getting == "q":
+    	if line != "" and line[0] != "[" and line[0] != "#":
+    		queries.append((line.rstrip('\n')).replace(' ', ''))
+    if line == '[Probabilities]':
+   		getting = "p"
+    if line == '[Nodes]':
+    	getting = "n"
+    if line == '[Queries]':
+    	getting = "q"
 
 final_nodes = parseNodes(nodes)
 parseProbabilities(final_nodes, probabilities)
